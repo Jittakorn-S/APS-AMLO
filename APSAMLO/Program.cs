bf551b2c42d359ecf62b2fd8937b476c02b04305
Jittakorn Srisin <60041997+Jittakorn-S@users.noreply.github.com>
2025-07-09 12:38:21 +0800

    Add files via upload

---
+++ APSAMLO/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+
+namespace APSAMLO
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<Application>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var Service = serviceProvider.GetService<Application>();
+            await Service.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# APS_AMLO_API
+
+## Description
+
+Retrieve data from a REST API and save it into a CSV file
